2fbbf8fe232598c33f3195c8f959155531e79363
duruyao <duruyao@gmail.com>
2022-01-20 15:38:09 +0800

    feat: add install.sh for installing tools of DockerDK

---
+++ install.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+## date:   2022-01-20
+## author: duruao@gmail.com
+## desc:   install tools of DockerDK
+
+function show_usage() {
+  cat <<EOF
+Usage: $0 [-h|--help] [--prefix PREFIX]
+
+-h, --help
+    Display this help message.
+
+--prefix PREFIX
+    Specify an installation directory (default: /usr/local/bin).
+
+EOF
+}
+
+prefix="/usr/local/bin"
+
+## parse arguments
+while (($#)); do
+  case "$1" in
+  -h | --help)
+    show_usage
+    exit 0
+    ;;
+
+  --prefix)
+    if [ -z "$2" ]; then
+      echo "Error: $1 requires a non empty argument" >&2
+      show_usage >&2
+      exit 1
+    elif [ ! -d "$2" ]; then
+      echo "Error: No such directory: $2" >&2
+      show_usage >&2
+      exit 1
+    else
+      prefix="$2"
+      shift 2
+    fi
+    ;;
+
+  --* | -* | *)
+    echo "Error: Unknown flag: $1" >&2
+    show_usage >&2
+    exit 1
+    ;;
+  esac
+done
+
+set -x
+
+chmod +x "${PWD}"/tools/*
+
+cp "${PWD}"/tools/* "${prefix}"/
