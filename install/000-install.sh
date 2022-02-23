5990789b4df90035f086f9fc6c920d69bb3978ee
duruyao <duruyao@gmail.com>
2022-02-23 11:32:58 +0800

    refactor: refactor 000-install.sh

--- install/000-install.sh
+++ install/000-install.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
-if [ -n "$(command -v "$0")" ]; then
-  install_dir="$(dirname "$(command -v "$0")")"
-else
-  install_dir="$(dirname "$(readlink -f "$0")")"
-fi
+install_dir="$(dirname "$(readlink -f "$0")")"
 
 for file in "${install_dir}"/*.sh; do
-  if [ "${file}" != "${install_dir}/000-install-list.sh" ]; then
+  if [ "${file}" != "${install_dir}"/000-install.sh ]; then
     printf "\n\033[1;32;34m%s\033[m\n\n" "bash -x ${file}"
     bash -x "${file}"
   fi
