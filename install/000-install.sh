f509f54768465135fac21ac2903d80822cafb83f
duruyao <duruyao@gmail.com>
2022-02-23 17:36:21 +0800

    refactor: add self-defined function traceln()

--- install/000-install.sh
+++ install/000-install.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+function traceln() {
+  printf "\033[1;36m%s\n\033[m" "${1}"
+}
+
 install_dir="$(dirname "$(readlink -f "$0")")"
 
 for file in "${install_dir}"/*.sh; do
   if [ "${file}" != "${install_dir}"/000-install.sh ]; then
-    printf "\n\033[1;32;34m%s\033[m\n\n" "bash -x ${file}"
+    traceln "bash -x ${file}"
     bash -x "${file}"
   fi
 done
