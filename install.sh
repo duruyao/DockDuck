e6f33eb5fe06adf67107298a65bc0f0200b17ecf
duruyao <duruyao@gmail.com>
2022-05-16 13:34:34 +0800

    feat: add debug mode for developer

--- install.sh
+++ install.sh
@@ -39,6 +39,7 @@ Usage: $0 [OPTIONS]
 Install dock-duck to the specified directory
 
 Options:
+  -d, --debug                   Install a debug version
   -h, --help                    Display this help message
       --prefix STRING           Specify an installation directory (default: "${HOME}/.duck")
 
@@ -48,10 +49,16 @@ EOF
 }
 
 prefix="${HOME}/.duck"
+debug=false
 
 ## parse arguments
 while (($#)); do
   case "$1" in
+  -d | --debug)
+    debug=true
+    shift 1
+    ;;
+
   -h | --help)
     show_usage
     exit 0
@@ -83,10 +90,12 @@ chmod +x "${PWD}"/app/*
 chmod +x "${PWD}"/tools/*
 
 dk_home="${prefix}"
+dk_app_home="${dk_home}/app"
+dk_img_home="${dk_home}/img"
 dk_data_home="${dk_home}/data"
 dk_tools_home="${dk_home}/tools"
 
-if [ -f "${dk_home}/dk" ] || [ -d "${dk_tools_home}" ]; then
+if [ -d "${dk_app_home}" ] || [ -d "${dk_img_home}" ] || [ -d "${dk_tools_home}" ]; then
   warning_ln "Warning: There may already be a dock-duck (v$("${dk_home}"/dk -v | grep -o "[0-9]\+\.[0-9]\+\.[0-9]\+")) in ${dk_home}"
   # shellcheck disable=SC2162
   read -p "Do you want to continue installing the current version? [Y/n] " install_dk
@@ -101,29 +110,35 @@ echo "Installing ..."
 echo
 
 mkdir -p "${dk_home}"
+mkdir -p "${dk_app_home}"
+mkdir -p "${dk_img_home}"
 mkdir -p "${dk_data_home}"
 mkdir -p "${dk_tools_home}"
 
-cp "${PWD}"/app/* "${dk_home}"/
-cp "${PWD}"/tools/* "${dk_tools_home}"/
-cp "${PWD}"/img/*.logo "${dk_data_home}"/
+cp "${PWD}"/app/* "${dk_app_home}"
+cp "${PWD}"/img/* "${dk_img_home}"
+cp "${PWD}"/tools/* "${dk_tools_home}"
 
-line="export DK_HOME=\"${dk_home}\""
-if ! grep -Fxq "${line}" "${HOME}"/.bashrc; then
-  echo "${line}" >>"${HOME}"/.bashrc
+if ${debug}; then
+  rm -f "${dk_app_home}"/*
+  ln -s "${PWD}"/app/dk "${dk_app_home}"/dk
+  rm -f "${dk_tools_home}"/*
+  # shellcheck disable=SC2207
+  dk_tools=($(ls ./tools))
+  for dk_tool in "${dk_tools[@]}"; do
+    ln -s "${PWD}"/tools/"${dk_tool}" "${dk_tools_home}"/"${dk_tool}"
+  done
 fi
 
-line="export PATH=\"\$DK_HOME:\$PATH\""
-if ! grep -Fxq "${line}" "${HOME}"/.bashrc; then
-  echo "${line}" >>"${HOME}"/.bashrc
-fi
+bash_config="
+### DockDuck Bash Config Begin
+export DK_HOME=\"${dk_home}\" # DockDuck Bash Config
+export PATH=\"\$DK_HOME/app:\$PATH\" # DockDuck Bash Config
+source \"\$DK_HOME/tools/dk-complete\" # DockDuck Bash Config
+### DockDuck Bash Config End
+"
 
-line="source \$DK_HOME/tools/dk-complete" # FIXME: use variable instead of value
-if ! grep -Fxq "${line}" "${HOME}"/.bashrc; then
-  echo "${line}" >>"${HOME}"/.bashrc
-fi
-
-source "${HOME}"/.bashrc
+echo "${bash_config}" >>"${HOME}"/.bashrc
 
 echo "Install dock-duck to ${dk_home}"
 if [ -n "$(command -v tree)" ]; then
